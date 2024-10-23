1ed19b3466cd012b7cdf36e613d5d41927a95465
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-10-23 18:13:57 +0300

    pricing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,38 @@
 import featureIcon from "../assets/feature-icon.png";
 
-export default function Card({ children, plan }) {
+export default function Card({ children, plan, btnStyles }) {
+  const btnClasses = "btn bg-primary btn-lg w-100 mt-3 fs-6 fw-bold " + btnStyles;
+
   return (
-    <div className="col-12 col-sm-6 col-lg-4 col-xxl-3">
+    <div className="col-12 col-sm-6 col-lg-4 px-5">
       <div className="card border-0">
-        <div className="card-body text-center py-4">
-          <h4 className="card-title">{plan.name}</h4>
+        <div className="card-body text-center p-3">
+          <div className="pb-4 d-flex flex-row-reverse">{children}</div>
+          <h4 className="card-title fs-6">{plan.name}</h4>
           <div className=" my-4">
-            <p className="d-inline display-5 fw-bold">{plan.price} </p>
+            <p className="d-inline fs-1 fw-bold">{plan.price} </p>
             <p className="d-inline text-muted">/{plan.user}</p>
           </div>
-            <hr className="text-muted my-4"></hr>
-          <ul>
+          <hr className="text-line my-4"></hr>
+          <ul className="p-0">
             {plan.features.map((feature) => {
               return (
-                <span className="d-block text-start">
-                  <img className="d-inline me-2" src={featureIcon}></img>
-                  <p className="d-inline text-start" key={Math.random()}>
+                <span key={Math.random()} className="d-block text-start py-1">
+                  <img
+                    className="d-inline me-2"
+                    height={24}
+                    width={24}
+                    src={featureIcon}
+                  ></img>
+                  <small className="d-inline">
                     {feature}
-                  </p>
+                  </small>
                 </span>
               );
             })}
           </ul>
-          <a href="#" className="btn text-light bg-success btn-lg w-100 mt-3">
-            Get Started
+          <a href="#" className={btnClasses}>
+            <small>Get Started</small>
           </a>
         </div>
       </div>
--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -40,16 +40,20 @@ const enterprisePlan = {
 
 export default function Price() {
   return (
-    <section id="pricing" className="bg-light mt-5">
+    <section id="pricing" className="bg-purple bg-opacity-25 py-4">
       <div className="container-lg">
-        <div className="text-center">
-          <h2>Simple pricing for everyone</h2>
-          <p className="lead text-mutted">Choose a plan and get started</p>
+        <div className="text-center pt-5">
+          <h2 className="fw-bold">Simple pricing for everyone</h2>
+          <p className="text-mutted">Choose a plan and get started</p>
         </div>
-        <div className="row my-5 align-items-center justify-content-between">
-          <Card plan={personalPlan}></Card>
-          <Card plan={professionalPlan}> </Card>
-          <Card plan={enterprisePlan}> </Card>
+        <div className="row m-5 align-items-center justify-content-between">
+          <Card plan={personalPlan} btnStyles="bg-opacity-25" />
+          <Card plan={professionalPlan} btnStyles="text-light">
+            <small className="bg-dark text-light rounded py-1 px-3">
+              Popular
+            </small>
+          </Card>
+          <Card plan={enterprisePlan} btnStyles="bg-opacity-25" />
         </div>
       </div>
     </section>
