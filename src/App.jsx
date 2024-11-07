7266fc75862663dc25791c0f1ccccf162151198a
RadwaAhmed4869 <48102202+RadwaAhmed4869@users.noreply.github.com>
2024-11-07 13:02:50 +0200

    responsive banner

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Shop from "./components/Shop.jsx";
 import Products from "./components/Products.jsx";
 import Banner from "./components/Banner.jsx";
 
-import leftBanner from "./assets/left-banner.png";
-import rightBanner from "./assets/right-banner.png";
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import Quality from "./components/Quality.jsx";
@@ -19,24 +17,7 @@ function App() {
       <Header />
       <NavBar />
 
-      <div className="row g-2">
-        <Banner
-          image={leftBanner}
-          header="T-shirt printing
-made easy."
-          paragraph="Create your design for your
-online business"
-          btnCaption="Create T-shirt"
-        />
-        <Banner
-          image={rightBanner}
-          header="Marina
-Test"
-          paragraph="Print shirts for yourself or your
-creative works"
-          btnCaption="Shop Now"
-        />
-      </div>
+      <Banner />
 
       <Quality />
 
